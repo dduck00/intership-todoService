7d0f5defb7376b216003c150d8d9063a3738dd78
이상덕 <USER@AD01091260>
2019-07-05 14:47:35 +0900

    DAO

--- src/main/java/dao/TodoDao.java
+++ src/main/java/dao/TodoDao.java
@@ -16,18 +16,14 @@ public class TodoDao {
 
 	private static String SELECT_SQL_QU = "select id, title, name, sequence, type, regdate "
 		+ "from todo "
-		+ "order by regdate desc "
-		+ "select id, title, name, sequence, type, regdate "
-		+ "from todo "
-		+ "where type = ? "
-		+ "order by regdate desc";
+		+ "order by regdate desc ";
 
-	private static String UPDATE_SQL_QU_TO_DOING = "update todo "
-		+ "set type = 'DOING' "
-		+ "where id = 1; ";
-	private static String UPDATE_SQL_QU_TO_DONE = "update todo "
-		+ "set type = 'DONE' "
-		+ "where id = 1;";
+	private static String UPDATE_SQL_QU = "update todo "
+		+ "set type = ? "
+		+ "where id = ?; ";
+
+	private static String INSERT_SQL_QU = "insert into todo(title, name, sequence) "
+		+ "values(?, ?, ?);";
 
 	public TodoDao() {
 		try {
@@ -38,21 +34,33 @@ public class TodoDao {
 	}
 
 	public int addTodo(TodoDto input_data) {
-		return 1;
+		int result = 0;
+		try (Connection conn = DriverManager.getConnection(dburl, dbuser, dbpasswd);
+			PreparedStatement ps = conn.prepareStatement(INSERT_SQL_QU)) {
+			ps.setString(1, input_data.getTitle());
+			ps.setString(2, input_data.getName());
+			ps.setInt(3, input_data.getSequence());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	public List<TodoDto> getTodos() {
 		List<TodoDto> data_from_database = new ArrayList<>();
 		try (Connection conn = DriverManager.getConnection(dburl, dbuser, dbpasswd);
 			PreparedStatement ps = conn.prepareStatement(SELECT_SQL_QU)) {
-
 			try (ResultSet rs = ps.executeQuery()) {
-
 				while (rs.next()) {
-					String description = rs.getString("DOING");
-					int id = rs.getInt("role_id");
-					//					TodoDto data = new TodoDto(id, description);
-					//					list.add(role); // list에 반복할때마다 Role인스턴스를 생성하여 list에 추가한다.
+					TodoDto data = new TodoDto(
+						rs.getLong("id"),
+						rs.getString("name"),
+						rs.getString("regdate"),
+						rs.getInt("sequence"),
+						rs.getString("title"),
+						rs.getString("type"));
+					data_from_database.add(data);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -60,10 +68,19 @@ public class TodoDao {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		return null;
+		return data_from_database;
 	}
 
 	public int updateTodo(TodoDto update_data) {
-		return 1;
+		int result = 0;
+		try (Connection conn = DriverManager.getConnection(dburl, dbuser, dbpasswd);
+			PreparedStatement ps = conn.prepareStatement(UPDATE_SQL_QU)) {
+			ps.setString(1, update_data.getType());
+			ps.setLong(2, update_data.getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
