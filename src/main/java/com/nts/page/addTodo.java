0906addf52a1d9f28b06e88a3a4654be39cb2a38
이상덕 <USER@AD01091260>
2019-07-08 12:17:27 +0900

    일정추가 서블릿 추가

---
+++ src/main/java/com/nts/page/addTodo.java
@@ -0,0 +1,36 @@
+package com.nts.page;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.nts.dao.TodoDao;
+import com.nts.dto.TodoDto;
+
+@WebServlet("/addTodo")
+public class addTodo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final TodoDao DB_CONNECTOR = new TodoDao();
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		response.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
+		TodoDto todo = new TodoDto();
+		todo.setName(request.getParameter("name"));
+		todo.setSequence(Integer.parseInt(request.getParameter("sequence")));
+		todo.setTitle(request.getParameter("title"));
+		DB_CONNECTOR.addTodo(todo);
+		PrintWriter out = response.getWriter();
+		out.println(todo.getName());
+		out.println(todo.getSequence());
+		out.println(todo.getTitle());
+	}
+
+}
