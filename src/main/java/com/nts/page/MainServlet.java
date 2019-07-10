b6aed8a965e194368586e2b4ab5d6e3b07e2942d
이상덕 <USER@AD01091260>
2019-07-10 17:43:46 +0900

    foreach 수정

--- src/main/java/com/nts/page/MainServlet.java
+++ src/main/java/com/nts/page/MainServlet.java
@@ -37,11 +37,6 @@ public class MainServlet extends HttpServlet {
 			List<TodoDto> Dones = new ArrayList<TodoDto>();
 
 			for (TodoDto todo : listTodo) {
-
-				if (todo == null) {
-					continue;
-				}
-
 				if (todo.getType().equals("TODO")) {
 					Todos.add(todo);
 				} else if (todo.getType().equals("DOING")) {
